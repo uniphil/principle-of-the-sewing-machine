1b7294c1435d13b0397076c7e44053dc8d4379d7
phil <uniphil@gmail.com>
2019-02-06 14:09:17 -0500

    updated for current setup

--- projector_speed_pot.ino
+++ projector_speed_pot.ino
@@ -1,10 +1,15 @@
-#define POT A0
-#define _MOTOR_PWM 3  // arduino pin number
-#define _MOTOR_REVERSE 4  // arduino pin number
-#define _MOTOR_MAX_POWER 127  // 8-bit pwm range
-#define _MOTOR_POWER_STEP_DT 4  // milliseconds
+// PINS
+#define PEDAL A0
+#define LED_MONITOR A1
+#define LED 10
+#define _MOTOR_PWM 5  // arduino pin number
+#define _MOTOR_REVERSE A3  // arduino pin number
 
-// never write to these variables directly
+// Behaviour constants
+#define _MOTOR_MAX_POWER 127  // 8-bit pwm range
+#define _MOTOR_POWER_STEP_DT 5  // milliseconds
+
+// never write to these variables directly -- some routines take care of them
 uint8_t _motor_speed = 0;
 bool _motor_reversing = false;
 
@@ -19,6 +24,8 @@ bool _motor_reversing = false;
 void drive(uint8_t new_speed, bool reverse = false) {
   while (_motor_reversing != reverse ||
          _motor_speed     != new_speed) {
+    delay(_MOTOR_POWER_STEP_DT);
+
     if (_motor_speed == 0) {
       // only flip the direction signal when the motor is at rest
       _motor_reversing = reverse;
@@ -34,21 +41,9 @@ void drive(uint8_t new_speed, bool reverse = false) {
     }
 
     analogWrite(_MOTOR_PWM, map(_motor_speed, 0, 255, 0, _MOTOR_MAX_POWER));
-    delay(_MOTOR_POWER_STEP_DT);
   }
 }
 
-volatile unsigned long last = 0;
-
-
-void hi() {
-  unsigned long now = millis();
-  if (now == last) return;
-  unsigned long dt = now - last;
-  if (dt < 10) return;
-  last = now;
-  Serial.println(1.0 / ((float)dt / 1000));
-}
 
 void setup() {
   // motor pin setup
@@ -58,17 +53,12 @@ void setup() {
   digitalWrite(_MOTOR_REVERSE, LOW);
   // end motor setup
 
-  pinMode(2, INPUT_PULLUP);
-  last = millis();
-  attachInterrupt(digitalPinToInterrupt(2), hi, FALLING);
-
   Serial.begin(9600);
 }
 
 void loop() {
-  uint16_t reading = analogRead(POT);
+  uint16_t reading = analogRead(PEDAL);
   drive(reading >> 2);  // reduce 10-bit to 8-bit
 //  Serial.println(reading);
-  delay(2);
 }
 
